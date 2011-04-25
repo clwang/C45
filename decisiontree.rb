95f27e1bec5b27270c8da8560a895e03b9dc8b87
Charles Wang <cwang@realpractice.com>
2011-04-24 20:04:30 -0700

    created a menu, various options, and selecting single test records

--- decisiontree.rb
+++ decisiontree.rb
@@ -7,7 +7,7 @@ require "id3tree.rb"
 # The Split data method will randomly split the data 80/20 ratio
 def split_data(data, test_data, training_data)
   size = data.count
-  train_size = (size * 0.70).round
+  train_size = (size * 0.90).round
   test_size = size - train_size
   data = data.shuffle
   counter = 0
@@ -45,9 +45,64 @@ def print_output(dtree, test_data)
   end
 end
 
+def single_result(data, dtree)
+  puts "Displaying test data set......\n"
+  counter = 0
+  count = data.count
+  data.each do |d|
+    print "#{counter += 1} ) "
+    puts d.inspect
+  end
+  print "\n------------------------------------------------------------------------\n"
+  begin
+    print "Input a record number to be tested : "
+    record = gets.chomp
+    if record.to_i <= 0 || record.to_i > count
+      puts "Invalid record, please try again!"
+    end
+  end while record.to_i <= 0 || record.to_i > count 
+  print "You have selected record : "
+  puts data[record.to_i-1].inspect
+  puts "Processing.........\n"
+  predict = dtree.predict(data[record.to_i-1])
+  actual = data[record.to_i-1].last
+  if predict == actual
+    match = true
+  else
+    match = false
+  end
+  puts "Predicted: #{predict}   |    Actual: #{actual}    |  Match: #{match}"
+  return true 
+end
+
+def menu
+  print "\n------------------------------------------------------------------------\n"
+  print "OPTIONS:\n"
+  print "\t G : Graph \n"
+  print "\t S : Select a record to test\n"  
+  print "\t O : Display Available Menu Options\n"
+  print "\t Q : Quit program\n"        
+  print "------------------------------------------------------------------------\n\n"
+end
+
+def print_intro
+  print "------------------------------------------------------------------------\n"
+  print "\t\t Welcome to the ID3 Decision Tree Program \n"
+  print "\t\t CPSC 531 - Spring 2011 \n"
+  print "\t\t Developed by Jisu Ha and Charles Wang\n"      
+  print "------------------------------------------------------------------------\n\n"
+end
+
+def graph(dtree)
+  puts "Graphing......"
+  dtree.graph("graph")
+  puts "Graph completed... check for graph.png in the program directory"
+end
+
 attributes = ["buying", "maint", "doors", "persons", "lug_boot", "safety"]
 training_data, test_data, fixed_data = [],[],[]
 
+print_intro
 print "Input your sample data: "
 file_name = gets.chomp
 data = File.open(File.expand_path("test_data/" + file_name))
@@ -66,7 +121,24 @@ test_data = foo[1]
 
 id3_tree = DTree::ID3Tree.new(training_data, attributes)
 id3_tree.begin
-
 print_output(id3_tree,test_data)
+menu
 
-id3_tree.graph("test")
+begin
+  print "Input your menu selection: "
+  input = gets.chomp
+  case input
+    when 'G','g' 
+      graph(id3_tree)
+    when 'S','s'
+      single_result(test_data, id3_tree)
+    when 'O','o' 
+      menu
+    when 'Q', 'q'
+      puts "Quitting......"
+      puts "Goodbye!!"
+    else
+      puts "Not a valid input, please try again!"
+      menu
+  end
+end while !input.eql? 'q' || 'Q'
